5d40f00ed23a50788c4b24440d87bdb939724bed
hhernandez4 <hhernandez4@babson.edu>
2017-03-13 09:08:47 -0400

    kept working on creating databse

--- SHI_dbcreation.sql
+++ SHI_dbcreation.sql
@@ -1,4 +1,5 @@
-﻿CREATE DATABASE SHIdb;
+﻿/*SHI Cocina by Sofia Cosio, Isabel Esteve and Helena Hernandez*/
+CREATE DATABASE SHIdb;
 GO
 
 USE SHIdb;
@@ -7,7 +8,7 @@ GO
 -- Create the customer and order tables
 CREATE TABLE Customer(
   CustomerID bigint NOT NULL PRIMARY KEY,
-  LatName varchar(50) NOT NULL,
+  LastName varchar(50) NOT NULL,
   FirstName varchar(50) NOT NULL,
   Email varchar(15),
   PhoneNumber varchar(10) NOT NULL,
@@ -19,45 +20,44 @@ CREATE TABLE Customer(
   Country varchar(3)
   );
 
-CREATE TABLE Course(
-  CourseID bigint NOT NULL PRIMARY KEY,
-  CourseTitle varchar(50) NOT NULL,
-  Instuctor varchar(50) NOT NULL);
+CREATE TABLE Employees(
+  EmployeeID bigint NOT NULL PRIMARY KEY,
+  LName varchar(50) NOT NULL,
+  FName varchar(50) NOT NULL,
+  EmployeeType varchar(8) NOT NULL,
+  Email varchar(15),
+  PhoneNumber varchar(10) NOT NULL,
+  AddressLine1 varchar(50),
+  AddressLine2 varchar(20),
+  City varchar(10),
+  State varchar(10),
+  ZipCode bigint(5),
+  Country varchar(3),
+  LocationID bigint NOT NULL FOREIGN KEY REFERENCES Locations(LocationID) 
+  );
 
 CREATE TABLE CourseEnrollment(
-  EnrollmentID bigint NOT NULL PRIMARY KEY,
-  StudentId bigint NOT NULL,
-  CourseID bigint NOT NULL,
-  Grade float NOT NULL);
+  
 
 -- Create the relationship: the first FK in CourseEnrollment
 ALTER TABLE CourseEnrollment ADD CONSTRAINT FK_CourseEnrollment_Student 
 FOREIGN KEY (StudentId) REFERENCES Student(StudentID);
 GO
 
--- We will use designer view to create another relationship: the second FK in CourseEnrollment
+
 
 
 -- Add a few students
 INSERT INTO Student (StudentID, FirstName, LastName) VALUES 
 (1, 'Beong Jo', 'Kang'),
-(2, 'Kelsey', 'Claflin'),
-(3, 'Helena', 'Hernandez'),
-(4, 'Upasana', 'Roy'),
-(5, 'Waree', 'Rungsiriwat'),
-(6, 'Shazeem', 'Siddiqi'),
-(7, 'Connie', 'Li'),
-(8, 'Hunter', 'Schilb'),
-(9, 'Angela', 'Tsung'),
+
 (10, 'David', 'Butsko');
 
 
 -- Add a few courses
 INSERT INTO Course(CourseID, CourseTitle, Instuctor) VALUES 
 (1, 'MIS3545-BUSINESS INTELLIGENCE AND DATA ANALYTICS', 'Zhi'),
-(2, 'MIS3690-WEB TECHNOLOGIES', 'Shankar'),
-(3, 'QTM3000-CASE STUDIES IN BUSINESS ANALYTICS', 'Nathan'),
-(4, 'QTM3625-FINANCIAL SIMULATION', 'Dessi'),
+
 (5, 'SME2012-MANAGING INFORMATION TECH AND SYSTEMS', 'Clare');
 GO
 
@@ -67,29 +67,10 @@ WHERE CourseTitle like 'SME%';
 GO 
 
 
--- We will use data view to add grade data
 
-
--- Some queries
 SELECT * FROM Student;
 SELECT * FROM Course;
 SELECT * FROM CourseEnrollment;
 
 GO
-
--- Create a handy view summarizing students' grades
-CREATE VIEW vwStudentGradeSummary WITH SCHEMABINDING AS
- SELECT
-   s.StudentID, s.FirstName, s.LastName, 
-   ISNULL(COUNT(ce.CourseID), 0) AS NumberOfCourses,
-   ISNULL(SUM(ce.Grade), 0) AS TotalGrade
-  FROM
-   dbo.Student AS s
-   LEFT OUTER JOIN dbo.CourseEnrollment AS ce ON s.StudentID = ce.StudentId
-  GROUP BY
-   s.StudentID, s.FirstName, s.LastName
-GO
-
-
-SELECT * FROM vwStudentGradeSummary;
 
